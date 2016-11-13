3e0b60a3b4661902d525647230fc7ad918d324ae
Arindam Chaudhuri <ksharindam@gmail.com>
2016-11-13 07:52:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 install quartz-browser.py /usr/local/bin
 install quartz_common.py /usr/local/bin
-install settings_dialog.py bookmarks_dialog.py bookmarkparser.py /usr/local/bin
+install settings_dialog.py bookmarks_dialog.py bookmarkparser.py download_manager.py /usr/local/bin
