0f2de0d4677ef97ec7f46d44274cbdab0da364c2
Arindam Chaudhuri <ksharindam@gmail.com>
2017-04-13 14:35:41 +0530

    Create quartz directory

---
+++ quartz/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Name = Quartz Browser
+version = 1.8.5
+Dependency = python-qt4, libnotify-bin
+Description = A Light Weight Internet Browser
+Features =  Change User agent to mobile/desktop
+            Print Page to PDF
+            Save page as JPG, html
+            Turn Javascript, Load Images on/off
+            Find Text inside page
+            Easy accessible toolbar
+            Tabbed browsing
+            Download Manager with pause/resume support
+Last Update : 
+            Pause or Stop options in downloads (instead of only pause).
+            Window icon added.
+            Print preview dialog added.
+            new Download confirmation dialog designed.
+            Download names are correctly resolved.
+   v1.8.4 : Download filenames are no longer surrounded by quotes.
+            Shows version in window title.
+            Download manager now saves data each time after 96kB is received.
+            Printer settings changed (now it adds title and creator)
+            Set alternate row colors in downloads/bookmarks/history
+            Code size reduced by removing Main.defaultsettings()
+
+"""
+# TODO : 
+#       multiple search engines
+#       save downloads immediately after download starts
