98205ae5201d4ab0dd7b5502216179a5d196fca6
Arindam Chaudhuri <ksharindam@gmail.com>
2017-04-13 15:24:52 +0530

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm /usr/local/bin/quartz-browser.py /usr/local/bin/quartz_common.py /usr/local/bin/settings_dialog.py /usr/local/bin/bookmarks_dialog.py /usr/local/bin/bookmark_parser.py /usr/local/bin/download_manager.py
