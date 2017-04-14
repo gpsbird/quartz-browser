22cc43ea6411af037b1db76204a7d46a74f7428b
Arindam Chaudhuri <ksharindam@gmail.com>
2017-04-14 11:12:44 +0530

    gitignore updated

--- quartz/__init__.py
+++ quartz/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Name = Quartz Browser
-version = 1.8.5
+version = 1.8.6
 Dependency = python-qt4, libnotify-bin
 Description = A Light Weight Internet Browser
 Features =  Change User agent to mobile/desktop
@@ -16,14 +16,8 @@ Last Update :
             Pause or Stop options in downloads (instead of only pause).
             Window icon added.
             Print preview dialog added.
-            new Download confirmation dialog designed.
+   v1.8.5   new Download confirmation dialog designed.
             Download names are correctly resolved.
-   v1.8.4 : Download filenames are no longer surrounded by quotes.
-            Shows version in window title.
-            Download manager now saves data each time after 96kB is received.
-            Printer settings changed (now it adds title and creator)
-            Set alternate row colors in downloads/bookmarks/history
-            Code size reduced by removing Main.defaultsettings()
 
 """
 # TODO : 
